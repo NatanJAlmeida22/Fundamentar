d0e7dc5e97e0e81180172b39035718e5ef904648
Natan José de Almeida <natanalmeida158@hotmail.com>
2022-06-11 17:57:08 -0300

    Initial commit

---
+++ JS/comentario.js
@@ -0,0 +1,13 @@
+// Aqui eu tenho um comentário em linha
+
+/*
+    Aqui eu tenho um comentário multi
+    linhas
+
+    o código abaixo escreve alguma
+    mensagem no devtools
+
+*/
+
+console.log("Bem vindos ao Starter");
+console.log("Não iria ignorar");
