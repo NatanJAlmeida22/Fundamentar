85dac75cbe625acc04c0246a7732d430cd9d65e6
Natan Jose de Almeida <natanalmeida158@hotmail.com>
2022-06-13 10:01:36 -0300

    class condicionais

---
+++ JS/condicionais_repetição/catch.js
@@ -0,0 +1,16 @@
+// throw
+
+function sayMyName(name = "") {
+    if (name == "") {
+        throw new Error("Nome é obrigatório")
+    }
+
+    console.log("Depois do erro")
+}
+
+// try...catch
+try {
+    sayMyName()
+} catch (e) {
+    console.log(e);
+}
