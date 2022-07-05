c0e9a6d9a9e7219b973186c35e0fa50644e52728
Natan José de Almeida <natanalmeida158@hotmail.com>
2022-07-05 14:21:57 -0300

    final do módulo

---
+++ NODE/require.js
@@ -0,0 +1,9 @@
+// Módulos nativos
+// const path = require("path")
+
+// console.log(path.basename("/Users/strater/docs.js"))
+
+// Meus módulos
+const myModule = require("./exports")
+
+console.log(myModule)
